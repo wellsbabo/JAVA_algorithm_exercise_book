789ab4a9c5aa8f48ebaff48bb9457787dbcacac3
wellsbabo <wellsbabo@naver.com>
2024-10-31 08:54:59 +0900

    Update BestTimeToBuyAndSellStock.java

--- Grind75/BestTimeToBuyAndSellStock.java
+++ Grind75/BestTimeToBuyAndSellStock.java
@@ -2,24 +2,24 @@ package Grind75;
 
 public class BestTimeToBuyAndSellStock {
     public static void main(String[] args){
-        maxProfit(new int[]{1,0,0});
+        int result = maxProfit(new int[]{7,6,4,3,1});
+        System.out.println("result = " + result);
     }
 
     public static int maxProfit(int[] prices) {
-        int result = 0;
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
         
-        for(int i=0; i<prices.length-1; i++){
-            for(int j=i; j<prices.length; j++){
-                if(prices[j] <= prices[i]){
-                    continue;
-                }
-                int tmp = prices[j] - prices[i];
-                if(tmp > result){
-                    result = tmp;
+        for(int price : prices){
+            if(price < min){
+                min = price;
+            }else{
+                if((price-min) > maxProfit){
+                    maxProfit = price - min;
                 }
             }
         }
-        System.out.println("result = " + result);
-        return result;
+        return maxProfit;
     }
+
 }
